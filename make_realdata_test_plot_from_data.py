5f74443ad77a1ddbafcee2bf4b3bf2c4a89f145c
Glenn Palmer <glennpalmer55@gmail.com>
2021-01-14 21:56:32 -0600

    updating plot formatting

--- make_realdata_test_plot_from_data.py
+++ make_realdata_test_plot_from_data.py
@@ -2,22 +2,21 @@ import matplotlib
 from package import MakePlot as mp
 import numpy as np
 
-model = "GPR_Bayesian"
-dataset = "Diffusion"
-phase = Test
+models = ["GPR_Bayesian", "RF", "LR"]
+dataset = "Perovskite"
 save_plot = True
 
-unscaled_model_errors = np.load('data_for_paper_plots/{}/{}/Test/Test_model_errors_unscaled.npy'.format(dataset, model))
-scaled_model_errors = np.load('data_for_paper_plots/{}/{}/Test/Test_model_errors_scaled.npy'.format(dataset, model))
-residuals = np.load('data_for_paper_plots/{}/{}/Test/Test_residuals.npy'.format(dataset, model))
-a = np.load('data_for_paper_plots/{}/{}/Test/a.npy'.format(dataset, model))
-b = np.load('data_for_paper_plots/{}/{}/Test/b.npy'.format(dataset, model))
+for model in models:
+    unscaled_model_errors = np.load('data_for_paper_plots/{}/{}/Test/Test_model_errors_unscaled.npy'.format(dataset, model))
+    scaled_model_errors = np.load('data_for_paper_plots/{}/{}/Test/Test_model_errors_scaled.npy'.format(dataset, model))
+    residuals = np.load('data_for_paper_plots/{}/{}/Test/Test_residuals.npy'.format(dataset, model))
+    a = np.load('data_for_paper_plots/{}/{}/Test/a.npy'.format(dataset, model))
+    b = np.load('data_for_paper_plots/{}/{}/Test/b.npy'.format(dataset, model))
 
-# scaled_model_errors = model_errors * a[0] + b[0]
 
-MP = mp.MakePlot()
+    MP = mp.MakePlot()
 
-MP.make_rstat(residuals, unscaled_model_errors, "{}, {}, Unscaled".format(model, dataset), save=save_plot, file_name='Supplemental_Info/{}/5-Fold/{}/Test_Plots/unscaled_rstat.png'.format(dataset, model))
-MP.make_rstat(residuals, scaled_model_errors, "{}, {}, Scaled".format(model, dataset), save=save_plot, file_name='Supplemental_Info/{}/5-Fold/{}/Test_Plots/scaled_rstat.png'.format(dataset, model))
-MP.make_rve_with_bin_counts_and_slope_1_line(residuals, unscaled_model_errors, "{}, {}, Unscaled".format(model, dataset), save=save_plot, file_name='Supplemental_Info/{}/5-Fold/{}/Test_Plots/unscaled_RvE_with_counts.png'.format(dataset, model))
-MP.make_rve_with_bin_counts_and_slope_1_line(residuals, scaled_model_errors, "{}, {}, Scaled".format(model, dataset), save=save_plot, file_name='Supplemental_Info/{}/5-Fold/{}/Test_Plots/scaled_RvE_with_counts.png'.format(dataset, model))
+    MP.make_rstat(residuals, unscaled_model_errors, "{}, {}, Unscaled".format(model, dataset), save=save_plot, file_name='Supplemental_Info/{}/5-Fold/{}/Test_Plots/unscaled_rstat.png'.format(dataset, model))
+    MP.make_rstat(residuals, scaled_model_errors, "{}, {}, Scaled".format(model, dataset), save=save_plot, file_name='Supplemental_Info/{}/5-Fold/{}/Test_Plots/scaled_rstat.png'.format(dataset, model))
+    MP.make_rve_with_bin_counts_and_slope_1_line(residuals, unscaled_model_errors, "{}, {}, Unscaled".format(model, dataset), save=save_plot, file_name='Supplemental_Info/{}/5-Fold/{}/Test_Plots/unscaled_RvE_with_counts.png'.format(dataset, model))
+    MP.make_rve_with_bin_counts_and_slope_1_line(residuals, scaled_model_errors, "{}, {}, Scaled".format(model, dataset), save=save_plot, file_name='Supplemental_Info/{}/5-Fold/{}/Test_Plots/scaled_RvE_with_counts.png'.format(dataset, model))
--- make_synth_cv_plot_from_data.py
+++ make_synth_cv_plot_from_data.py
@@ -2,19 +2,20 @@ import matplotlib
 from package import MakePlot as mp
 import numpy as np
 
-model = "GPR_Bayesian"
+models = ["RF", "LR", "GPR_Bayesian", "GPR"]
 save_plot = True
 
-model_errors = np.load('data_for_paper_plots/Friedman_500/{}/Test/Test_model_errors.npy'.format(model))
-residuals = np.load('data_for_paper_plots/Friedman_500/{}/Test/Test_residuals.npy'.format(model))
-a = np.load('data_for_paper_plots/Friedman_500/{}/Test/a.npy'.format(model))
-b = np.load('data_for_paper_plots/Friedman_500/{}/Test/b.npy'.format(model))
+for model in models:
+    model_errors = np.load('data_for_paper_plots/Friedman_500/{}/CV/CV_model_errors.npy'.format(model))
+    residuals = np.load('data_for_paper_plots/Friedman_500/{}/CV/CV_residuals.npy'.format(model))
+    a = np.load('data_for_paper_plots/Friedman_500/{}/CV/a.npy'.format(model))
+    b = np.load('data_for_paper_plots/Friedman_500/{}/CV/b.npy'.format(model))
 
-scaled_model_errors = model_errors * a[0] + b[0]
+    scaled_model_errors = model_errors * a[0] + b[0]
 
-MP = mp.MakePlot()
+    MP = mp.MakePlot()
 
-MP.make_rstat(residuals, model_errors, "{}, Friedman 500, Unscaled".format(model), save=save_plot, file_name='Supplemental_Info/Friedman_500/5-Fold/{}/Test_Plots/unscaled_rstat.png'.format(model))
-MP.make_rstat(residuals, scaled_model_errors, "{}, Friedman 500, Scaled".format(model), save=save_plot, file_name='Supplemental_Info/Friedman_500/5-Fold/{}/Test_Plots/scaled_rstat.png'.format(model))
-MP.make_rve_with_bin_counts_and_slope_1_line(residuals, model_errors, "{}, Friedman 500, Unscaled".format(model), save=save_plot, file_name='Supplemental_Info/Friedman_500/5-Fold/{}/Test_Plots/unscaled_RvE_with_counts.png'.format(model))
-MP.make_rve_with_bin_counts_and_slope_1_line(residuals, scaled_model_errors, "{}, Friedman 500, Scaled".format(model), save=save_plot, file_name='Supplemental_Info/Friedman_500/5-Fold/{}/Test_Plots/scaled_RvE_with_counts.png'.format(model))
+    MP.make_rstat(residuals, model_errors, "{}, Friedman 500, Unscaled, Single CV Split".format(model), save=save_plot, file_name='Supplemental_Info/Friedman_500/5-Fold/{}/CV_Plots/unscaled_rstat.png'.format(model))
+    MP.make_rstat(residuals, scaled_model_errors, "{}, Friedman 500, Scaled, Single CV Split".format(model), save=save_plot, file_name='Supplemental_Info/Friedman_500/5-Fold/{}/CV_Plots/scaled_rstat.png'.format(model))
+    MP.make_rve_with_bin_counts_and_slope_1_line(residuals, model_errors, "{}, Friedman 500, Unscaled, Single CV Split".format(model), save=save_plot, file_name='Supplemental_Info/Friedman_500/5-Fold/{}/CV_Plots/unscaled_RvE_with_counts.png'.format(model))
+    MP.make_rve_with_bin_counts_and_slope_1_line(residuals, scaled_model_errors, "{}, Friedman 500, Scaled, Single CV Split".format(model), save=save_plot, file_name='Supplemental_Info/Friedman_500/5-Fold/{}/CV_Plots/scaled_RvE_with_counts.png'.format(model))
